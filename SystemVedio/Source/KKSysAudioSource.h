6c80157e216c48bbeefa390d9e6ab2363d48654c
KeSen <superkesen@gmail.com>
2017-08-19 10:56:04 +0800

    集成Bell_SDK

---
+++ SystemVedio/Source/KKSysAudioSource.h
@@ -0,0 +1,13 @@
+//
+//  KKSysAudioSource.h
+//  SystemVedio
+//
+//  Created by sen.ke on 2017/8/18.
+//  Copyright © 2017年 sen.ke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KKSysAudioSource : NSObject
+
+@end
---
+++ SystemVedio/Source/KKSysAudioSource.m
@@ -0,0 +1,13 @@
+//
+//  KKSysAudioSource.m
+//  SystemVedio
+//
+//  Created by sen.ke on 2017/8/18.
+//  Copyright © 2017年 sen.ke. All rights reserved.
+//
+
+#import "KKSysAudioSource.h"
+
+@implementation KKSysAudioSource
+
+@end
